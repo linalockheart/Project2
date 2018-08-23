2b28b34493a0d6089f15ed2f0873ead930dd1f52
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-08-23 13:37:16 -0400

    come on

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,33 +1,58 @@
 var path = require("path");
-var request = require('request');
+var request = require("request");
+var NodeGeocoder = require("node-geocoder");
+
+var options = {
+  provider: "google",
+
+  // Optional depending on the providers
+  httpAdapter: "https", // Default
+  apiKey: "PkbrJL7xu1GhLJIDiKQ8bOuvgKb3LzAV", // for Mapquest, OpenCage, Google Premier
+  formatter: null // 'gpx', 'string', ...
+};
+
+var geocoder = NodeGeocoder(options);
+console.log(path);
+
+
+
+
 module.exports = function (app) {
 
-    app.post("/api", function (req, res) {
-       
-        newSearch = req.body;
+  app.post("/api", function (req, res) {
+
+    var newSearch = req.body;
+    var location = newSearch.location;
+    geocoder.geocode(location)
+      .then(function (res) {
+        var latitude = res[0].latitude.toFixed(2);
+        var longitude = res[0].longitude.toFixed(2);
+        console.log("lat and long " + latitude + " and   " + longitude);
+        return latitude + "," + longitude;
+      })
+      .then(function (result) {
         request({
-            url: 'https://api.foursquare.com/v2/venues/search',
-            method: 'GET',
-            qs: {
-              client_id: 'ILMLHZWCXE2UVCAMQCTI0IYDKLU4YLUQANSWQVNLPZJW0IY4',
-              client_secret: 'HO010YK2ASDUOAXEWIQCHH1XFCY3FLORRJIDZUJ0ZRIUHWHR',
-              ll: '39.9717,-75.1280',
-              query: newSearch.name,
-              v: '20180323',
-              limit: 1
-            }
-          }, function(err, result, body) {
-            if (err) {
-              console.error(err);
-            } else {
-              console.log(JSON.parse(body, null, 2));
-              return res.json(JSON.parse(body));
-            }
-          });
-        
+          url: "https://api.foursquare.com/v2/venues/search",
+          method: "GET",
+          qs: {
+            clientId: "ILMLHZWCXE2UVCAMQCTI0IYDKLU4YLUQANSWQVNLPZJW0IY4",
+            clientSecret: "HO010YK2ASDUOAXEWIQCHH1XFCY3FLORRJIDZUJ0ZRIUHWHR",
+            ll: result,
+            query: newSearch.name,
+            v: "20180323",
+            limit: 1
+          }
+        }, function (err, result, body) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(JSON.parse(body, null, 2));
+            return res.json(JSON.parse(body));
+          }
+        });
 
-       
 
-    });
+      });
+  });
 
-}
+};
